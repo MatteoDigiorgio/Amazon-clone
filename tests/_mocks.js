68df20981963d80ce249a5b03e978674ee569131
Matteo Digiorgio <matthew.digiorgio@gmail.com>
2023-01-03 13:43:24 +0100

    Mock built for getServerSideProps test

---
+++ tests/_mocks.js
@@ -0,0 +1,15 @@
+export const MockProductsList = [
+  {
+    id: 1,
+    title: "Title",
+    price: 234,
+    description: "Description",
+    category: "Category",
+    image: "https://image.jpeg",
+    rating: { count: 567, rate: 8.9 },
+  },
+];
+
+export default {
+  ProductsList: MockProductsList,
+};
