bc5e80ad827aba0c4e9555aacb0ead6a408e6e0e
Matteo Digiorgio <matthew.digiorgio@gmail.com>
2023-02-19 18:14:26 +0100

    Began development fetching orders from firebase

--- src/app/(main)/orders/page.tsx
+++ src/app/(main)/orders/page.tsx
@@ -1,22 +1,17 @@
-"use client";
+// "use client";
 import React from "react";
+import Items from "./Items";
 import styles from "./Orders.module.css";
-import { useSession } from "next-auth/react";
 
-function orders() {
-  const { data: session } = useSession();
+async function orders() {
   return (
     <div className={styles.orders_page}>
       <main>
         <h1>Your Orders</h1>
 
-        {session ? (
-          <h3>x Orders</h3>
-        ) : (
-          <h3>Please sign in to see your orders</h3>
-        )}
-
-        <div className={styles.order}></div>
+        <div className={styles.order}>
+          <Items orders={[]} />
+        </div>
       </main>
     </div>
   );
