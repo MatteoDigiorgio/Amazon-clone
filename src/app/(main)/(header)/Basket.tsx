fd38b0c171733b3ca59cdc426b59cc82787d3b22
Matteo <matthew.digiorgio@gmail.com>
2023-02-22 22:06:08 +0100

    Update Basket.tsx

--- src/app/(main)/(header)/Basket.tsx
+++ src/app/(main)/(header)/Basket.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import Link from "next/link";
 import styles from "./Header.module.css";
@@ -6,22 +5,22 @@ import { useSelector } from "react-redux";
 import { selectItems } from "../../../slices/basketSlice";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 
-function Basket() {
-  const Checkout = () => {
-    const items = useSelector(selectItems);
-    return (
-      <>
-        <div className={styles.basket}>
-          <p className={styles.counter}>{items.length}</p>
-          <Link title="Checkout" passHref href="/checkout">
-            <ShoppingCartIcon height={40} />
-            <p>Basket</p>
-          </Link>
-        </div>
-      </>
-    );
-  };
+function Checkout() {
+  const items = useSelector(selectItems);
+  return (
+    <>
+      <div className={styles.basket}>
+        <p className={styles.counter}>{items.length}</p>
+        <Link title="Checkout" passHref href="/checkout">
+          <ShoppingCartIcon height={40} />
+          <p>Basket</p>
+        </Link>
+      </div>
+    </>
+  );
+}
 
+function Basket() {
   return <Checkout />;
 }
 
