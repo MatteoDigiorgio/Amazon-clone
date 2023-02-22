7f96778868c18e942c1ca82810f02351e703e098
Matteo <matthew.digiorgio@gmail.com>
2023-02-22 23:57:46 +0100

    Update Basket.tsx

--- src/app/(main)/(header)/Basket.tsx
+++ src/app/(main)/(header)/Basket.tsx
@@ -7,24 +7,19 @@ import { selectItems } from "../../../slices/basketSlice";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 
 function Checkout() {
-  const items = useSelector(selectItems);
   
   return (
-    <>
-      <div className={styles.basket}>
-        <p className={styles.counter}>{items.length}</p>
-        <ShoppingCartIcon height={40} />
-        <p>Basket</p>
-      </div>
-    </>
+    <p>Basket</p>
   );
 }
 
 function Basket() {
   return (
+  <div className={styles.basket}>
     <Link href="/checkout" passHref>
         <Checkout />
     </Link>
+  </div>
   );
 }
 
